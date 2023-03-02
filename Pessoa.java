b9ed9267f0ba11bdc13d344f159bba6d6ac0a007
Matheush2ds <saomatheus1223@gmail.com>
2023-03-02 11:00:21 -0300

    primeiro

---
+++ Pessoa.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Pessoa {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner (System.in);
+
+        int x = 0.
+        int y = 0;
+
+        System.out.println("a soma é " + x + y);
+        
+    }
+}
